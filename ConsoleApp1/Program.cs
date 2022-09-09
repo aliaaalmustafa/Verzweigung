85094cf11019a1a26aa50b6b1c91a18a2ac51f17
aliaa.a2 <aliaa.a2@P-0G-0B111-0429>
2022-09-09 12:17:45 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Aufgabe 1
+
+Console.WriteLine("Bitte schreiben Sie zwei Zahlen!");
+Console.Clear();
+
+Console.WriteLine("Bitte schreiben Sie nun die erste Zahl!");
+var zahl1 = Console.ReadLine();
+short short_zahl1 = Convert.ToInt16(zahl1);
+Console.Clear();
+
+Console.WriteLine("Bitte schreiben Sie nun die zweite Zahl!");
+var zahl2 = Console.ReadLine();
+short short_zahl2 = Convert.ToInt16(zahl2);
+Console.Clear();
+
+
+Console.WriteLine(Math.Min(short_zahl2, short_zahl1));
+
+
+//Aufgabe 2
+
